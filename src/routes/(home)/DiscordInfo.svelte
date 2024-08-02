b2b466daa93181530d28a1c0a0256e32a4b4a7ec
Subhadeep <codevizag@gmail.com>
2024-08-02 11:03:56 +0530

    Updated Discord Presence Images

--- src/routes/(home)/DiscordInfo.svelte
+++ src/routes/(home)/DiscordInfo.svelte
@@ -62,16 +62,60 @@
 		{#if fetchAsset}
 			<p class="flex items-center text-sm">
 				{#if fetchAsset}
-				<img
-						src="https://cdn.discordapp.com/app-assets/{fetchAsset.application_id}/{fetchAsset.assets.large_image}.png"
-						alt=""
-						class="w-5 h-5 mr-1"
-					/>
-					<!-- <img
-						src="https://{fetchAsset.assets.large_image.split('https/')[1]}"
+					<img src={
+							(fetchAsset.name === "Visual Studio Code" || 
+							 fetchAsset.name === "Visual Studio" ||
+							 fetchAsset.name === "Zed" ||
+							 fetchAsset.name === "PhpStorm" ||
+							 fetchAsset.name === "WebStorm" ||
+							 fetchAsset.name === "IntelliJ IDEA" ||
+							 fetchAsset.name === "IntelliJ IDEA Ultimate" ||
+							 fetchAsset.name === "IntelliJ IDEA Community" ||
+							 fetchAsset.name === "Jetbrains IDE" ||
+							 fetchAsset.name === "PyCharm" ||
+							 fetchAsset.name === "PyCharm Professional" ||
+							 fetchAsset.name === "PyCharm Community" ||
+							 fetchAsset.name === "Android Studio" ||
+							 fetchAsset.name === "VEGAS Pro" ||
+							 fetchAsset.name === "League of Legends" ) ?
+							`https://cdn.discordapp.com/app-assets/${fetchAsset.application_id}/${fetchAsset.assets.large_image}.png`
+
+							: fetchAsset.name === "Prime Video" ?
+							`https://cdn.discordapp.com/app-assets/705139844883677224/705140011946737806.webp`
+
+							: fetchAsset.name === "VALORANT" ? 
+							`https://cdn.discordapp.com/app-icons/700136079562375258/e55fc8259df1548328f977d302779ab7.webp` 
+
+							: fetchAsset.name === "Genshin Impact" ?
+							`https://cdn.discordapp.com/app-icons/762434991303950386/eb0e25b739e4fa38c1671a3d1edcd1e0.webp`
+
+							: fetchAsset.name === "Grand Theft Auto V" ?
+							`https://cdn.discordapp.com/app-icons/356876176465199104/069d9f4871b5ebd2f62bd342ce6ba77f.webp`
+
+							: fetchAsset.name === "PUBG" ?
+							`https://cdn.discordapp.com/app-icons/356873622985506820/d8cd6c5b1fe5b64ad2e0f660238eb43e.webp`
+
+							: fetchAsset.name === "Counter-Strike 2" ?
+							`https://cdn.discordapp.com/app-icons/1158877933042143272/558f5a26ecb3b17c3dea3d15c1df537a.webp`
+
+							: fetchAsset.name === "Minecraft" && fetchAsset.assets.small_image ?
+							`https://${fetchAsset.assets.small_image.split('https/')[1]}"`
+
+							: (fetchAsset.name === "Tom Clancy's Rainbow Six Siege" || fetchAsset.name === "Rainbow Six Siege")?
+							`https://cdn.discordapp.com/app-assets/445956193924546560/446301881636225042.png`
+
+							: fetchAsset.name === "Fortnite" ?
+							`https://cdn.discordapp.com/app-assets/${fetchAsset.application_id}/${fetchAsset.assets.small_image}.png`
+
+							: fetchAsset.name === "Call of Duty®: WWII" ?
+							`https://cdn.discordapp.com/app-assets/${fetchAsset.application_id}/${fetchAsset.assets.large_image}.png`
+
+							// Premid
+							: `https://${fetchAsset.assets.large_image.split('https/')[1]}`
+							}
 						alt=""
 						class="w-8 h-8 mr-1"
-					/> -->
+					/>
 				{/if}
 
 				<span class="line-clamp-1 break-all">
