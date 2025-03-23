a3916f324ab0b3840df0d6b75c04b5cf91075609
Subhadeep <codevizag@gmail.com>
2025-03-23 18:57:48 +0530

    Updated Projects ✨

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,34 +11,35 @@
 	<p class="mt-4 mb-8 text-gray-400">Some of my projects.</p>
 
 	<Project
-		name="PDF Compressor"
-		href="https://pdfc.dinokage.in"
-		src="/images/projects/nimi.png"
+		name="RDP Datacenter"
+		href="https://rdpdatacenter.cloud"
+		src="https://res.cloudinary.com/ddvheihbd/image/upload/f_auto,q_auto/v1/assets/rdp-dc"
 	>
-		A React application which uses AWS Services to compress PDFs without losing quality.
+	Building Cloud Platform in Public. <br>Ship with ease & speed.
 	</Project>
 
 	<Project
-		name="Gajala - All-in-One Discord Bot"
-		href="https://gajala.dinokage.in"
+		name="CodePulse"
+		href="https://code.rdpdatacenter.in"
 		src="/images/projects/debating.png"
 	>
-		Poems? Music? Moderation? Gajala has it all. Maa vadu multi talented.
+	Real-Time Pair Programming Platform where developers can collaborate in real-time with an AI assistant.
 	</Project>
 
 	<Project
-		name="Valorant Credentials Manager"
-		href="https://vcm.dinokage.in"
+		name="Bakes & Blooms"
+		href="https://blooms.phpxcoder.in"
 		src="/images/projects/pseudocode.png"
 	>
-		A custom account manager which sorts my Valorant accounts based on various criteria such as skins, rank, region etc.
+	Expose your Discord presence and activities to a RESTful API and WebSocket in less than 10 seconds.
 	</Project>
 
 	<Project
-		name="Weather API Example"
-		href="https://karnawww.netlify.app"
-		src="/images/projects/boids.png"
+		name="Lanyard API"
+		href="https://api.phpxcoder.in"
+		src="https://camo.githubusercontent.com/796470d30c99e768269058f9cb1146a20b1da396f88d5c35f77eb0181d9a3aca/68747470733a2f2f73746f726167652e676f6f676c65617069732e636f6d2f6c616e796172642f7374617469632f6c616e7961726474656d706c6f676f2e706e67"
 	>
-		A simple web application which implements OpenWeatherMap API to fetch weather based on browser location or user input.
+	Expose your Discord Presence to a RESTful API and WebSocket in less than 10 seconds.
+	Built with Elixir & Redis.
 	</Project>
 </main>
